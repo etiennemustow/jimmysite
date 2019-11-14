e620beda046058b98b2461713d24035d7edd793e
Etienne Mustow <etienne.o.mustow@gmail.com>
2019-11-14 18:27:59 +0000

    Procfile and PORT const added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-var port = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
   res.render('index', {title: "Home" });
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
 });
 //
