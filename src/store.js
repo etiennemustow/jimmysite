c59d62504e5e04270e619d216ba8147865d6ddbb
Etienne Mustow <etiennemustow@pc-129-98.customer.ask4.lan>
2021-07-25 22:30:56 +0100

    remove unnecessary packages

--- src/store.js
+++ src/store.js
@@ -1,5 +1,3 @@
-import { createRef } from "react"
-import { Vector3 } from "three"
 
 const state = {
   
@@ -34,7 +32,7 @@ const state = {
       css: "gallery__item gallery__item--"
     },
     {
-      header: "British Vogue \“Now You See Me\”",
+      header: "British Vogue “Now You See Me”",
       image: "/ballroomscene.jpeg",
       text1: "directed by Iggy London",
       text2: "production design by Jimmy Van Twest",
@@ -517,21 +515,6 @@ smart_energy: [
     },
   ]
 },
-  stripes: [
-    { offset: 0, color: "#000", height: 13 },
-    { offset: 6.3, color: "#000", height: 20 }
-  ],
-  diamonds: [
-    { x: 0, offset: 0.15, pos: new Vector3(), scale: 0.6, factor: 1.8 },
-    { x: 2, offset: 1.1, pos: new Vector3(), scale: 0.8, factor: 2.1 },
-    { x: -5, offset: 2, pos: new Vector3(), scale: 0.8, factor: 2.5 },
-    { x: 0, offset: 3.2, pos: new Vector3(), scale: 0.8, factor: 1.75 },
-    { x: 0, offset: 4, pos: new Vector3(), scale: 0.8, factor: 2.5 },
-    { x: 2, offset: 5.5, pos: new Vector3(), scale: 1.25, factor: 0.85 },
-    { x: -5, offset: 7, pos: new Vector3(), scale: 0.8, factor: 2 },
-    { x: 0, offset: 8, pos: new Vector3(), scale: 1.5, factor: 6 }
-  ],
-  top: createRef()
 }
 
 export default state
