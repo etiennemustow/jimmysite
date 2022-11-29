a20829be1179c83045d6ec47d2b059e8201a479e
Etienne Mustow <etienne.mustow@simplybusiness.co.uk>
2022-11-29 22:23:40 +0000

    all works but handle with care carousel

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ function NavBar({ selected_work, contact }) {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
+          <li className="nav-item">
             <a className="nav-link" id={selected_work} href="/selected_work">Selected Work</a>
           </li>
           <li className="nav-item">
@@ -43,27 +43,27 @@ function Contact() {
       <NavBar contact="active_tab" />
       <div className="background-no-scroll">
 
-      <div className="contact-page">
-        <div className="boxed">
-          <div className="contact-text">
-            <div className="centered-text">
-              <h2 className="contact-heading">About</h2>
-              <br></br>
-              <h2>I've worked within the art department for Commercial, Music Videos, Fashion and Film since March 2019. 
-                In working a variety of roles, notably, Set Designer, Art Assistant, Set Builder and Render Artist, I'm constantly learning and always strive to create exceptional sets. 
-                Based In London.</h2>
-              <br></br>
-              <div><h2>Get in touch:</h2></div>
-              <br></br>
-              <div className=""><a className="" href={`mailto:jimmyvantwest@gmail.com`}><h2 className="black">jimmyvantwest@gmail.com</h2></a></div>
-              <div className=""><a className="" href="tel:+447588893374"><h2 className="black">+44 (0) 758 889 3374</h2></a></div>
-              <a href="https://www.instagram.com/jimmyvantwest"><img className="contact-icon" src={state.icons.instagram}></img></a>
-              <br></br>
-            </div>
+        <div className="contact-page">
+          <div className="boxed">
+            <div className="contact-text">
+              <div className="centered-text">
+                <h2 className="contact-heading">About</h2>
+                <br></br>
+                <h2>I've worked within the art department for Fashion, Commercial, Music Videos, and Film since March 2019.
+                  In working a variety of roles, notably, Set Designer, Art Assistant, Set Builder and Render Artist, I'm constantly learning and always strive to create exceptional sets.
+                  Based In London.</h2>
+                <br></br>
+                <div><h2>Get in touch:</h2></div>
+                <br></br>
+                <div className=""><a className="" href={`mailto:jimmyvantwest@gmail.com`}><h2 className="black">jimmyvantwest@gmail.com</h2></a></div>
+                <div className=""><a className="" href="tel:+447588893374"><h2 className="black">+44 (0) 758 889 3374</h2></a></div>
+                <a href="https://www.instagram.com/jimmyvantwest"><img className="contact-icon" src={state.icons.instagram}></img></a>
+                <br></br>
+              </div>
 
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </>)
 }
@@ -75,11 +75,11 @@ function SelectedWork() {
   const [isOpenProdDesign0, setOpenProdDesign0] = useState(false)
   const [isOpenProdDesign1, setOpenProdDesign1] = useState(false)
   const [isOpenProdDesign2, setOpenProdDesign2] = useState(false)
-  const [isOpenProdDesign3, setOpenProdDesign3] = useState(false)  
-  const [isOpenProdDesign4, setOpenProdDesign4] = useState(false)  
-  const [isOpenProdDesign5, setOpenProdDesign5] = useState(false)  
+  const [isOpenProdDesign3, setOpenProdDesign3] = useState(false)
+  const [isOpenProdDesign4, setOpenProdDesign4] = useState(false)
+  const [isOpenProdDesign5, setOpenProdDesign5] = useState(false)
   const [isOpenProdDesign6, setOpenProdDesign6] = useState(false)
-  const [isOpenProdDesign7, setOpenProdDesign7] = useState(false)  
+  const [isOpenProdDesign7, setOpenProdDesign7] = useState(false)
   const [isOpenProdDesign8, setOpenProdDesign8] = useState(false)
   const [isOpenProdDesign9, setOpenProdDesign9] = useState(false)
   const [isOpenProdDesign10, setOpenProdDesign10] = useState(false)
@@ -130,6 +130,59 @@ function SelectedWork() {
     setOpenProdDesign10(boolean)
   }
 
+  function openModal0(index) {
+    console.log(index.key2)
+    document.getElementById(index.key2).style.display = "block";
+  }
+
+  function closeModal0(index) {
+    document.getElementById(index.key2).style.display = "none";
+  }
+
+
+  function videoPanel(index) {
+    return (
+      <React.Fragment key={index.action}>
+        {console.log(index.action)}
+        <ModalVideo height="700px" width="700px" channel={index.channel} autoplay isOpen={is_action[index.action]} videoId={index.video} onClose={() => prodDesignActions[index.action](false)} />
+        <figure className={index.css + index.action} onClick={(e) => {
+          e.stopPropagation()
+          prodDesignActions[index.action](true)
+        }}>
+          <div className="overlay">
+
+            <img className="gallery__img" src={index.image}></img>
+            <div className="text-header">{index.header}</div>
+            <div className="text-footer">{index.text1}</div>
+            <div className="text-footer2">{index.text2}</div>
+
+          </div>
+
+        </figure>
+      </React.Fragment>)
+  }
+
+  function photoPanel(index) {
+    return (
+      <>
+        <React.Fragment key={index.action}>
+
+          <RenderArtCarousel index={index} action_function={openModal0} image_class="gallery__img vogue" />
+
+          <div id={index.key2} className="modal">
+            <span onClick={(e) => {
+              e.stopPropagation()
+              closeModal0(index)
+            }} className="close cursor" >&times;</span>
+            <div className="modal-content-render-art">
+              <Carousel pictures_location={index.gallery} carousel_id="carousel_apartment" />
+            </div>
+          </div>
+        </React.Fragment>
+
+      </>
+    )
+  }
 
 
   const prodDesignActions = [prodDesign0Action, prodDesign1Action, prodDesign2Action, prodDesign3Action, prodDesign4Action, prodDesign5Action, prodDesign6Action, prodDesign7Action, prodDesign8Action, prodDesign9Action, prodDesign10Action]
@@ -142,32 +195,19 @@ function SelectedWork() {
 
       <div className="gallery">
 
-        {selected_work.map((index) =>
-          <React.Fragment key={index.action}>
-            <ModalVideo height="700px" width="700px" channel={index.channel} autoplay isOpen={is_action[index.action]} videoId={index.video} onClose={() => prodDesignActions[index.action](false)} />
-            <figure className={index.css + index.action} onClick={(e) => {
-              e.stopPropagation()
-              prodDesignActions[index.action](true)
-            }}>
-              <div className="overlay">
-
-                <img className="gallery__img" src={index.image}></img>
-                <div className="text-header">{index.header}</div>
-                <div className="text-footer">{index.text1}</div>
-                <div className="text-footer2">{index.text2}</div>
-
-              </div>
-
-            </figure>
-          </React.Fragment>
+        {selected_work.map((object) => {
+          if (object.gallery == null) {
+            return videoPanel(object)
+          } else {
+            return photoPanel(object)
+          }
+        }
         )}
-
-
-
 
       </div>
     </>)
 }
+
 
 function Heading({ text }) {
   return (
@@ -177,6 +217,58 @@ function Heading({ text }) {
   )
 }
 
+function Carousel({ pictures_location, carousel_id }) {
+  const pictures = pictures_location
+  return (
+    <div id={carousel_id} className="carousel slide carousel-fade" data-ride="carousel">
+      <div className="carousel-inner">
+        {pictures.map((picture) => {
+          if (picture.action == 0) {
+            return (<div className="carousel-item active" key={picture.key + picture.action}>
+              <div className="crop">
+                <img className="d-block w-100" src={picture.image} alt="First slide"  ></img>
+              </div>
+            </div>)
+          } else {
+            return (<div className="carousel-item" key={picture.key + picture.action}>
+              <div className="crop">
+                <img className="d-block w-100" src={picture.image} alt="Second slide" ></img>
+              </div>
+            </div>)
+          }
+        }
+        )}
+
+      </div>
+      <a className="carousel-control-prev" href={'#' + carousel_id} role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href={'#' + carousel_id} role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+  )
+}
+
+function RenderArtCarousel({ index, action_function, image_class }) {
+  return (
+    <figure className={index.css + index.action} key={index.action_icon} onClick={(e) => {
+      e.stopPropagation()
+      { action_function(index) }
+    }}>
+      <div className="overlay">
+
+        <img className={image_class} src={index.image}></img>
+        <div className="text-header">{index.header}</div>
+        <div className="text-footer">{index.text1}</div>
+        <div className="text-footer2">{index.text2}</div>
+      </div>
+
+    </figure>
+  )
+}
 
 function Home() {
   return (
--- src/store.js
+++ src/store.js
@@ -52,54 +52,106 @@ const state = {
       css: "gallery__item gallery__item--"
     },
     {
+      header: "Jeshi - Universal Credit (Album Cover)",
+      image: "/selected_work/jeshi/jeshi.jpg",
+      text1: "photography by Francis Plummer",
+      text2: "set design by Jimmy Van Twest",
+      video: "569321719",
+      key2: 'jeshi',
+      channel: "vimeo",
+      action: 4,
+      css: "gallery__item gallery__item--",
+      gallery: [
+        {
+          header: "Jeshi - Universal Credit (Album Cover)",
+          key: "vogue",
+          key2: "jeshi",
+          image: "/selected_work/jeshi/jeshi_full.jpg",
+          text: "photography by Ivory Campbell",
+          action: 0,
+          action_icon: 0,
+          css: "gallery__item gallery__item--"
+        }
+    ]
+    },
+    {
+      header: "Obongjayar - Tinko Tinko",
+      image: "/selected_work/obongjayar.jpg",
+      text1: "Directed by Milo Blake",
+      text2: "set design by Jimmy Van Twest",
+      video: "9-BsluU-pYI",
+      channel: "youtube",
+      action: 5,
+      css: "gallery__item gallery__item--"
+    },
+    {
       header: "ELI - LOVER",
       image: "/selected_work/elilover.jpg",
       text1: "directed by Jade O'connor",
       text2: "production design by Jimmy Van Twest",
       video: "yp7H_VYHu18",
       channel: "youtube",
-      action: 4,
+      action: 6,
       css: "gallery__item gallery__item--"
     },
     {
-      header: "Finn Foxell- Make It Count",
+      header: "Handle With Care",
+      image: "/selected_work/handle_with_care/handle_with_care.jpeg",
+      text1: "photography by Ivory Campbell",
+      text2: "set design by Jimmy Van Twest",
+      video: "569321719",
+      channel: "vimeo",
+      key2: 'handle_with_care',
+      action: 7,
+      css: "gallery__item gallery__item--",
+      gallery: [
+        {
+        header: "Handle With Care",
+        key: "vogue",
+        key2: "handle_with_care",
+        image: "/selected_work/handle_with_care/handle_with_care_full.jpeg",
+        text: "photography by Ivory Campbell",
+        action: 0,
+        action_icon: 0,
+        css: "gallery__item gallery__item--"
+      },
+      {
+        header: "Handle With Care",
+        key: "vogue",
+        key2: "handle_with_care",
+        image: "/selected_work/handle_with_care/handle_with_care2.jpeg",
+        text: "photography by Ivory Campbell",
+        action: 1,
+        css: "gallery__item gallery__item--"
+      },
+      {
+        header: "Handle With Care",
+        key: "vogue",
+        key2: "handle_with_care",
+        image: "/selected_work/handle_with_care/handle_with_care3.jpeg",
+        text: "photography by Ivory Campbell",
+        action: 2,
+        css: "gallery__item gallery__item--"
+      },
+      {
+        header: "Handle With Care",
+        key: "vogue",
+        key2: "handle_with_care",
+        image: "/selected_work/handle_with_care/handle_with_care4.jpeg",
+        text: "photography by Ivory Campbell",
+        action: 3,
+        css: "gallery__item gallery__item--"
+      }
+    ]
+    },
+    {
+      header: "Finn Foxell - Make It Count",
       image: "/selected_work/makeitcount.jpg",
       text1:
       "directed by Milo Blake",
       text2: "production design by Jimmy Van Twest",
       video: "450474177",
       channel: "vimeo",
-      action: 5,
-      css: "gallery__item gallery__item--"
-    },
-    {
-      header: "Emma Jean Thackray - Say Something",
-      image: "/selected_work/emmajeansaysomething.jpeg",
-      text1: "directed by Avesta Keshtmand",
-      text2: "production design by Jimmy Van Twest & Sarah Asmail",
-      video: "kvh2OSrghEA",
-      channel: "youtube",
-      action: 6,
-      css: "gallery__item gallery__item--"
-    },
-    {
-      header: "TTRRUUCES - TTRRUUCES",
-      image: "/selected_work/truuuces.png",
-      text1: "directed by Maxim Kelly",
-      text2: "production design by Jimmy Van Twest",
-      video: "380030498",
-      channel: "vimeo",
-      action: 7,
-      css: "gallery__item gallery__item--"
-    },
-    {
-      header: "BXKS - Must Feel",
-      image: "/selected_work/bxksmustfeel.jpeg",
-      text1:
-      "directed by Sheena Brobbey",
-      text2: "production design by Jimmy Van Twest",
-      video: "d63EvToaaiI",
-      channel: "youtube",
       action: 8,
       css: "gallery__item gallery__item--"
     },
@@ -107,7 +159,7 @@ const state = {
       header: "Macmillan Cancer Research",
       image: "/selected_work/macmillan.jpeg",
       text1:
-      "Directed by Charlotte Regan",
+      "directed by Charlotte Regan",
       text2: "production design by Jimmy Van Twest",
       video: "Gzqu0Uc0exQ",
       channel: "youtube",
@@ -118,11 +170,21 @@ const state = {
       header: "Barclays - It All Starts With A Chance",
       image: "/selected_work/barclays.jpeg",
       text1:
-      "directed by Sheena Brobbey",
+      "directed by Charlotte Regan",
       text2: "production design by Jimmy Van Twest",
       video: "7zY0-EECd8o",
       channel: "youtube",
       action: 10,
+      css: "gallery__item gallery__item--"
+    },
+    {
+      header: "Sidemen x Topps",
+      image: "/selected_work/toppssidemen.png",
+      text1: "Directed by Rvbberduck",
+      text2: "production design by Jimmy Van Twest",
+      video: "739953910",
+      channel: "vimeo",
+      action: 11,
       css: "gallery__item gallery__item--"
     },
   ]
